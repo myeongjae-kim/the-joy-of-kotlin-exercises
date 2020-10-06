1b5ad37d9e7e3d44aa00d10a4203c2e6722aad16
Myeongjae Kim 김명재 <dev@myeongjae.kim>
2020-10-06 23:05:56 +0900

    Solve 6.1 and 6.2

---
+++ src/test/kotlin/chap6/Ch6ExercisesTest.kt
@@ -0,0 +1,75 @@
+package chap6
+
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.RuntimeException
+import kotlin.test.assertEquals
+
+class Ch6ExercisesTest {
+
+    sealed class Option<out A> {
+
+        abstract fun isEmpty(): Boolean
+
+        fun getOrElse(default: @UnsafeVariance A): A = when(this) {
+            None -> default
+            is Some -> this.value
+        }
+
+        fun getOrElse(default: () -> @UnsafeVariance A): A = when(this) {
+            None -> default()
+            is Some -> this.value
+        }
+
+        internal object None: Option<Nothing>() {
+
+            override fun isEmpty(): Boolean = true
+
+            override fun toString(): String = "None"
+
+            override fun equals(other: Any?): Boolean = other === None
+
+            override fun hashCode(): Int = 0
+
+        }
+
+        internal data class Some<out A>(internal val value: A): Option<A>() {
+
+            override fun isEmpty() = false
+        }
+
+        companion object {
+
+            operator fun <A> invoke(a: A? = null): Option<A> = when (a) {
+                null -> None
+                else -> Some(a)
+            }
+        }
+    }
+
+    @Nested
+    inner class Ex01 {
+        @Test
+        fun solve() {
+            assertEquals(Option(1).getOrElse(0), 1)
+            assertEquals(Option<Int>().getOrElse(0), 0)
+        }
+    }
+
+    fun <T: Comparable<T>> max(list: List<T>): Option<T> = Option(list.maxByOrNull { it })
+
+    fun getDefault(): Int = throw RuntimeException()
+
+    @Nested
+    inner class Ex02 {
+        @Test
+        fun solve() {
+            val max1 = max(listOf(3, 5, 7, 2, 1)).getOrElse(::getDefault)
+            assertEquals(max1, 7)
+
+            val exception = assertThrows<RuntimeException> { max(listOf<Int>()).getOrElse(::getDefault) }
+            assertEquals(exception.javaClass, RuntimeException::class.java)
+        }
+    }
+}
